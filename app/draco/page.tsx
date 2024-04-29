0f112c43793729b772241467596f4c22b25bc92d
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-04-30 02:58:38 +0530

    Update capture.tsx to use environment facing mode and adjust device motion threshold

--- app/draco/page.tsx
+++ app/draco/page.tsx
@@ -1,16 +1,12 @@
 import { nanoid } from "nanoid";
 import { AI } from "./chat/actions";
 import { Chat } from "./chat/chat";
-import { NoSleepToggle } from "@/components/no-sleep";
-import { Capture } from "@/components/capture";
 
 const Page = () => {
   const id = nanoid();
 
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Capture />
-      <NoSleepToggle />
       <Chat id={id} />
     </AI>
   );
--- components/capture.tsx
+++ components/capture.tsx
@@ -1,74 +1,15 @@
 "use client";
+import { useDeviceMotion } from "@/hooks/use-device-motion";
 import React, { useState, useRef, useEffect } from "react";
 import Webcam from "react-webcam";
 
 const videoConstraints: MediaTrackConstraints = {
   width: 1280,
   height: 720,
-  facingMode: "environment",
+  facingMode: "environment", // Rear camera
 };
-
-interface Acceleration {
-  x: number | null;
-  y: number | null;
-  z: number | null;
-}
-
-const useDeviceMotion = () => {
-  const [isActive, setIsActive] = useState(false);
-  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
-
-  useEffect(() => {
-    const threshold = 3; // Define a threshold for activation
-    let lastAcceleration: Acceleration = { x: 0, y: 0, z: 0 };
-
-    const handleMotionEvent = (event: DeviceMotionEvent) => {
-      const acceleration = event.accelerationIncludingGravity;
-
-      setPosition({
-        x: acceleration?.x ?? 0,
-        y: acceleration?.y ?? 0,
-        z: acceleration?.z ?? 0,
-      });
-      console.log(acceleration);
-      if (
-        acceleration &&
-        lastAcceleration.x !== null &&
-        lastAcceleration.y !== null &&
-        lastAcceleration.z !== null
-      ) {
-        let deltaX = Math.abs(lastAcceleration.x - (acceleration.x || 0));
-        let deltaY = Math.abs(lastAcceleration.y - (acceleration.y || 0));
-        let deltaZ = Math.abs(lastAcceleration.z - (acceleration.z || 0));
-
-        if (deltaX > threshold || deltaY > threshold || deltaZ > threshold) {
-          setIsActive(true);
-        } else {
-          setIsActive(false);
-        }
-      }
-
-      lastAcceleration = {
-        x: acceleration?.x ?? 0,
-        y: acceleration?.y ?? 0,
-        z: acceleration?.z ?? 0,
-      };
-    };
-
-    window.addEventListener("devicemotion", handleMotionEvent);
-
-    return () => {
-      window.removeEventListener("devicemotion", handleMotionEvent);
-    };
-  }, []);
-
-  return { isActive, position };
-};
-
-export default useDeviceMotion;
 
 export const Capture = () => {
-  console.log("Capture");
   const webcamRef = useRef<Webcam>(null);
   const [imageSrc, setImageSrc] = useState<string | null>(null);
   const [cameraActive, setCameraActive] = useState(false);
