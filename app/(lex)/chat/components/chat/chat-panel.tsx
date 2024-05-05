43bd09bb822efd4ddd737151e25936295ebea3ac
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-05 14:02:36 +0530

    refactor: Add example messages to chat component

--- app/(lex)/chat/components/chat/chat-panel.tsx
+++ app/(lex)/chat/components/chat/chat-panel.tsx
@@ -6,6 +6,7 @@ import type { AI } from "../../../../draco/chat/actions";
 import { nanoid } from "nanoid";
 import { UserMessage } from "../../../../draco/chat/rsc/user-message";
 import { PromptForm } from "./prompt-form";
+import { exampleMessages } from "../example-messages";
 
 export interface ChatPanelProps {
   id?: string;
@@ -29,36 +30,12 @@ export function ChatPanel({
   const { submitUserMessage } = useActions();
   const [shareDialogOpen, setShareDialogOpen] = React.useState(false);
 
-  const exampleMessages = [
-    {
-      heading: "What are the",
-      subheading: "trending memecoins today?",
-      message: `What are the trending memecoins today?`,
-    },
-    {
-      heading: "What is the price of",
-      subheading: "$DOGE right now?",
-      message: "What is the price of $DOGE right now?",
-    },
-    {
-      heading: "I would like to buy",
-      subheading: "42 $DOGE",
-      message: `I would like to buy 42 $DOGE`,
-    },
-    {
-      heading: "What are some",
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`,
-    },
-  ];
-
   return (
     <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
       <ButtonScrollToBottom
         isAtBottom={isAtBottom}
         scrollToBottom={scrollToBottom}
-      />
-
+      />    
       <div className="mx-auto sm:max-w-2xl sm:px-4">
         <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
           {messages.length === 0 &&
