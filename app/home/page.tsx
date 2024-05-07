d9e695288efe6be80b1be1a8939c9ee0c2947e13
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-07 10:54:54 +0530

    feat: Add home, notes, and profile pages

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import { CardList, SessionCard } from "./session-card";
+
+
+export default async function HomePage() {
+  return (
+    <div className="px-4 py-2">
+      <CardList />
+    </div>
+  );
+}
---
+++ app/notes/page.tsx
@@ -0,0 +1,3 @@
+export default async function NotesPage() {
+  return <div className="px-4 py-2"></div>;
+}
