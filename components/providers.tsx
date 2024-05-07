c05ed28bc64dbb5cba7adce4a121ad9d707aedba
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-07 13:39:51 +0530

    chore: Add QueryClientProvider for React Query integration

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,12 +2,19 @@
 import { Provider } from "jotai";
 import { SessionProvider } from "next-auth/react";
 import { NextIntlClientProvider } from "next-intl";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <Provider>
-        <NextIntlClientProvider locale="en">{children}</NextIntlClientProvider>
+        <NextIntlClientProvider locale="en">
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </NextIntlClientProvider>
       </Provider>
     </SessionProvider>
   );
