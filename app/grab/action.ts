f697463bc76807d91aad35ed1c409268ee35eccc
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-09 15:24:58 +0530

    feat: Update npm dependencies and add @postlight/parser for parsing HTML to Markdown

--- app/grab/action.ts
+++ app/grab/action.ts
@@ -1,27 +1,11 @@
 "use server";
-import axios from "axios";
-import { Readability } from "@mozilla/readability";
-import { JSDOM } from "jsdom";
-import Showdown from "showdown";
+import Parser from "@postlight/parser";
+import { NodeHtmlMarkdown, NodeHtmlMarkdownOptions } from "node-html-markdown";
 
 export async function fetchArticle(url: string) {
-  const response = await axios.get(url);
-  const html = response.data;
-  const dom = new JSDOM(html);
-  const document = dom.window.document;
-  const reader = new Readability(document);
-  const article = reader.parse();
-  const htmlSting = article?.content;
-  console.log(htmlSting);
-  const htmlNewDom = new JSDOM(htmlSting);
-  console.log(htmlNewDom);
-  const newDocument = htmlNewDom.window.document;
-  console.log(newDocument);
-  const convertor = new Showdown.Converter();
-  if (htmlSting) {
-    const markdown = convertor.makeMarkdown(htmlSting, newDocument);
-    console.log(markdown);
-    return markdown;
-  }
-  return null;
+  let result = await Parser.parse(url);
+  let content = result.content;
+  const converter = new NodeHtmlMarkdown();
+  const markdown = converter.translate(content);
+  return markdown;
 }
