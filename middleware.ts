bf7b108f9917800dc2e9fa9010c90b58568ea7a7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-13 21:48:14 +0530

    chore: Update middleware.ts to include additional protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,9 @@
 export { default } from "next-auth/middleware";
+
+const protectedRoutes = ["/protected", "home"];
+
+const arrayForMatcher = protectedRoutes.map((route) => `${route}/:path*`);
+
 export const config = {
-  matcher: ["/protected/:path*"],
+  matcher: ["/protected/:path*", "/protected/:path*"],
 };
