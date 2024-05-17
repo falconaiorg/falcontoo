bfcabe13a72322693ab99f97513b83b46f30ae83
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-17 19:02:18 +0530

    feat: Add article card component and display articles on home page

---
+++ app/(base)/home/article-card.tsx
@@ -0,0 +1,49 @@
+"use client";
+import type { Article } from "@prisma/client";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
+import Image from "next/image";
+
+const dummyArticleData = [
+  {
+    title: "How to use Lex in a very very long article title",
+    author: "FalconAI is a very very long author name",
+    thumbnail: "lex.png",
+  },
+  {
+    title: "How to use Lex",
+    author: "FalconAI",
+    thumbnail: "lex.png",
+  },
+];
+
+export const ArticleCard = ({ article }: { article: Article }) => (
+  <Card className="flex flex-row items-center justify-between px-1">
+    <CardHeader>
+      <CardTitle className="leading-5">{article.title}</CardTitle>
+      <CardDescription>{article.description}</CardDescription>
+    </CardHeader>
+    <Image
+      src={"/lex.png"}
+      alt="Thumbnail"
+      className="h-10 w-10"
+      width={10}
+      height={10}
+    />
+  </Card>
+);
+
+export const ArticleList = ({ articles }: { articles: Article[] }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      {articles.map((article) => (
+        <ArticleCard article={article} key={article.title} />
+      ))}
+    </div>
+  );
+};
--- app/(base)/home/page.tsx
+++ app/(base)/home/page.tsx
@@ -1,11 +1,18 @@
-import { CardList, SessionCard } from "./session-card";
-import { TRPCTest } from "./test-trpc";
+import { auth, getServerComponentSession } from "@/auth";
+import { ArticleList } from "./article-card";
+import { CardList } from "./session-card";
+import { queries } from "@/server/next";
+import { Suspense } from "react";
 
 export default async function HomePage() {
+  const { user } = await getServerComponentSession();
+  const articles = await queries.article.getArticlesbyUserId({
+    userId: user.id,
+  });
+
   return (
     <div className="px-4 py-2">
-      <TRPCTest />
-      <CardList />
+      <ArticleList articles={articles} />
     </div>
   );
 }
