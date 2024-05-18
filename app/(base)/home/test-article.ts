9efe523e035cf3ccf25ca701b9facb9a282a909a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-18 22:05:40 +0530

    feat: Add test articles data for home page

---
+++ app/(base)/home/test-article.ts
@@ -0,0 +1,28 @@
+type TestArtciles = {
+  title: string;
+  url: string;
+  text: string;
+};
+
+export const testArticles: TestArtciles[] = [
+  {
+    title: "Paul Graham's Blog",
+    url: "https://www.paulgraham.com/reddits.html",
+    text: "Paul Graham's articles",
+  },
+  {
+    title: "Substack",
+    url: "https://ideabox.substack.com/p/beyond-the-code-my-reflections-after?r=2pw9o",
+    text: "Substack articles",
+  },
+  {
+    title: "Medium with Code",
+    url: "https://medium.com/the-ai-forum/implementing-agentic-rag-using-langchain-b22af7f6a3b5",
+    text: "Medium articles",
+  },
+  {
+    title: "YC News",
+    url: "https://www.ycombinator.com/library/59-how-to-set-up-hire-and-scale-a-growth-strategy-and-team",
+    text: "YC News articles",
+  },
+];
