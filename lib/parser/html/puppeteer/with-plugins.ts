74144ee1f4135efe7abd288d5b3661a432ee17d2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-20 16:53:51 +0530

    feat: Add puppeteer with plugins for web scraping

---
+++ lib/parser/html/puppeteer/with-plugins.ts
@@ -0,0 +1,37 @@
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
+import BlockResourcesPlugin from "puppeteer-extra-plugin-block-resources";
+type BlockedType =
+  | "document"
+  | "stylesheet"
+  | "image"
+  | "media"
+  | "font"
+  | "script"
+  | "texttrack"
+  | "xhr"
+  | "fetch"
+  | "prefetch"
+  | "eventsource"
+  | "websocket"
+  | "manifest"
+  | "signedexchange"
+  | "ping"
+  | "cspviolationreport"
+  | "preflight"
+  | "other";
+
+puppeteer.use(StealthPlugin());
+puppeteer.use(
+  AdblockerPlugin({
+    blockTrackers: true,
+  }),
+);
+puppeteer.use(
+  BlockResourcesPlugin({
+    blockedTypes: new Set<BlockedType>(["font"]),
+  }),
+);
+
+export { puppeteer };
