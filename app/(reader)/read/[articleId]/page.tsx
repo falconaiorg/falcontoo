35bfc66a9da9ce99d6da623c00dce66f0505d56c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-28 02:36:46 +0530

    refactor: Update card component in testing page
    This commit updates the card component in the testing page to use the new Card, CardContent, CardDescription, and CardHeader components from the "@/components/ui/card" module. The previous implementation was replaced with a more modular and reusable approach, improving the maintainability and readability of the codebase.

--- app/(reader)/read/[articleId]/page.tsx
+++ app/(reader)/read/[articleId]/page.tsx
@@ -3,6 +3,15 @@ import { server } from "@/server/next";
 import MarkdownWithHighlight from "../../components/markdown/with-highlight";
 import { FilterButton } from "../../components/filter-btn";
 import { AnnotationDrawer } from "../../components/drawer/annotation-drawer";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
 const markdownText = `
 # Sample Markdown
@@ -25,20 +34,60 @@ export default async function ReadPage({
     userId: user.id,
   });
   return (
-    <div className="px-4 py-2">
-      <FilterButton />
-      <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">{article.content.title}</h1>
-        <p className="text-gray-500">{article.content.description}</p>
-      </div>
-      <div>
-        <h1>Markdown with Highlighted Words</h1>
-        <MarkdownWithHighlight
-          markdownText={article.content.markdown}
-          searchWords={searchWords}
-        />
-        <AnnotationDrawer />
-      </div>
+    <div className="flex flex-col space-y-3 px-4 py-4">
+      {/* <FilterButton /> */}
+      <Card className="dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative ">
+        <CardHeader className="py-4">
+          <CardTitle className="text-sm">Pre-reading</CardTitle>
+          <CardDescription>
+            Before you start reading, here is a summary of the article. You will
+            review stuff here. You can read your past annotations here. You can
+            read similar articles here.
+          </CardDescription>
+        </CardHeader>
+      </Card>
+
+      <Card className="flex flex-col gap-4">
+        <CardHeader>
+          <CardTitle>{article.content.title}</CardTitle>
+          {/* <p className="text-sm text-gray-500">{article.content.description}</p> */}
+        </CardHeader>
+        <CardContent>
+          <MarkdownWithHighlight
+            markdownText={article.content.markdown}
+            searchWords={searchWords}
+          />
+          <AnnotationDrawer />
+        </CardContent>
+      </Card>
+      <Card
+        className={cn(
+          "animate-shimmer inline-flex items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50",
+        )}
+      >
+        <CardHeader className="py-4">
+          <CardTitle className="text-sm">Stay in the Flow</CardTitle>
+          <CardDescription className="text-sm">
+            Sam altman has something similar to this in his blog. I think you
+            might like reading it.
+          </CardDescription>
+          <CardFooter className="flex justify-end">
+            <div className="font-serif text-sm font-medium">Save 4 mins</div>
+          </CardFooter>
+        </CardHeader>
+      </Card>
+      <Card className="flex flex-col gap-4">
+        <CardHeader>
+          <h1 className="font-bold">Make what people want.</h1>
+        </CardHeader>
+        <CardContent>
+          <MarkdownWithHighlight
+            markdownText={article.content.markdown}
+            searchWords={searchWords}
+          />
+          <AnnotationDrawer />
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,6 +1,13 @@
 import prisma from "@/prisma";
 import { getServerComponentSession } from "@/auth";
 import { Embed } from "./embed";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const aiText =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, velit ac aliquet ultrices, urna nisi tincidunt nunc, id lacinia nunc purus in justo. Nulla facilisi. Sed euismod, velit ac aliquet ultrices, urna nisi tincidunt nunc, id lacinia nunc purus in justo. Nulla facilisi.";
@@ -19,21 +26,19 @@ export default async function TestingPage() {
   //   },
   // });
 
-  const article = await prisma.article.findUnique({
-    where: {
-      id: testArticle,
-    },
-  });
-
-  if (!article) {
-    return <div>No article found</div>;
-  }
-
   return (
-    <>
-      nothing
-      <Embed article={article} />
-    </>
+    <Card className="dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative">
+      <CardHeader>
+        <CardTitle>Pre-reading</CardTitle>
+        <CardDescription>
+          Before you start reading, here is a summary of the article
+        </CardDescription>
+      </CardHeader>
+
+      <button className="animate-shimmer inline-flex h-12 items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        Shimmer
+      </button>
+    </Card>
     // <div
     //   className={cn({
     //     "select-none": hasSelection,
