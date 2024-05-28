97083bb8366aa826f772d75dc14359422b751a1e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-05-29 00:38:11 +0530

    test: grab error

--- app/grab/grab-article.tsx
+++ app/grab/grab-article.tsx
@@ -14,12 +14,15 @@ import { url as urlRouter } from "@/urls";
 import { useRouter } from "next/navigation";
 import to from "await-to-js";
 import Link from "next/link";
+import { parseArticle } from "@/server/parser";
+import { parseUrl } from "@/server/parser/url";
 export const testArticle =
   "https://substack.com/home/post/p-144117118?source=queue";
 
 export function GrabArticle({ url }: { url: string }) {
   const router = useRouter();
   const [isRedirecting, setIsRedirecting] = useState(false);
+  const [manualError, setManualError] = useState<string | null>(null);
 
   const {
     data: article,
@@ -47,6 +50,17 @@ export function GrabArticle({ url }: { url: string }) {
   });
 
   const createArticleAndRedirect = async () => {
+    const [error1, parsedUrl] = await to(parseUrl({ url: url }));
+    if (error1) {
+      setManualError("error parsing url");
+      return;
+    }
+    const [error2, testArticle] = await to(parseArticle({ url: parsedUrl }));
+    if (error2) {
+      setManualError("error parsing article");
+      return;
+    }
+
     const [err, article] = await to(createArticle({ url: url }));
     if (err) {
       console.log(err);
@@ -76,6 +90,7 @@ export function GrabArticle({ url }: { url: string }) {
 
   return (
     <div>
+      {manualError && <div>{manualError}</div>}
       {article && (
         <AlertDialog open={!!article}>
           <AlertDialogContent className="w-11/12">
