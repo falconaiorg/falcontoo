a85868704e5fc9c32824b42a2e78c0142ec5d9c0
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-06-09 14:28:59 +0530

    refactor: Add createArticle mutation to appRouter

--- packages/trpc/node/server.ts
+++ packages/trpc/node/server.ts
@@ -1,26 +1,48 @@
-import { EventEmitter } from "events";
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { z } from "zod";
 import SuperJSON from "superjson";
+import { parseUrl } from "@falcon/lib/parser/url";
+import { parseArticle } from "@falcon/lib/parser";
+import { saveArticle } from "@falcon/lib/server/next/article/save-article";
+import { getToken } from "next-auth/jwt";
+import prisma from "@falcon/prisma";
 
-export const createContext = ({
+const base64Secret = process.env.NEXTAUTH_SECRET;
+
+export const ZAddOrUpdateArticle = z.object({
+  url: z.string().url().optional(),
+});
+
+export const createContext = async ({
   req,
   res,
 }: trpcExpress.CreateExpressContextOptions) => {
-  const getUser = () => {
-    if (req.headers.authorization !== "secret") {
-      return null;
-    }
-    return {
-      name: "alex",
-    };
-  };
+  const userJWT = await getToken({ req, secret: base64Secret });
+  if (!userJWT) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Login to perform action",
+    });
+  }
+
+  if (!userJWT.email) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Email is required",
+    });
+  }
+
+  const user = await prisma.user.findFirstOrThrow({
+    where: {
+      email: userJWT.email,
+    },
+  });
 
   return {
     req,
     res,
-    user: getUser(),
+    user,
   };
 };
 
@@ -47,6 +69,27 @@ export const appRouter = t.router({
     .mutation(async (opts) => {
       // use your ORM of choice
       return { id: "1", name: opts.input.name };
+    }),
+  createArticle: t.procedure
+    .input(ZAddOrUpdateArticle)
+    .mutation(async ({ ctx, input }) => {
+      console.log("Creating article", input.url);
+      if (!input.url) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "URL is required",
+        });
+      }
+      console.log("Creating article", input.url);
+      const parsedUrl = await parseUrl({ url: input.url });
+      console.log("Parsed URL", parsedUrl);
+      const article = await parseArticle({ url: parsedUrl });
+      console.log("Parsed article", article);
+      const savedArticle = await saveArticle({
+        articleData: article,
+        userId: ctx.user.id,
+      });
+      return savedArticle;
     }),
 });
 
