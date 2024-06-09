a5a6ea3049e7ca6fd71f2197fbbbe1c2ff2c6028
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-06-09 14:29:27 +0530

    refactor: Remove unused imports and providers in providers.tsx

--- apps/falcontoo/components/providers.tsx
+++ apps/falcontoo/components/providers.tsx
@@ -4,9 +4,6 @@ import { SessionProvider } from "next-auth/react";
 import { NextIntlClientProvider } from "next-intl";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import TRPCProvider from "@falcon/trpc/next/client/provider";
-import ExpressTRPCProvider from "@falcon/trpc/node/client/provider";
-import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
-import { NextCookies } from "@/app/(base)/notes/page";
 import { CookieTokens } from "@/app/layout";
 
 const queryClient = new QueryClient();
@@ -23,15 +20,13 @@ export default function Providers({
   return (
     <SessionProvider>
       <TRPCProvider headers={headers}>
-        <ExpressTRPCProvider headers={headers} tokens={tokens}>
-          <Provider>
-            <NextIntlClientProvider locale="en">
-              <QueryClientProvider client={queryClient}>
-                {children}
-              </QueryClientProvider>
-            </NextIntlClientProvider>
-          </Provider>
-        </ExpressTRPCProvider>
+        <Provider>
+          <NextIntlClientProvider locale="en">
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </NextIntlClientProvider>
+        </Provider>
       </TRPCProvider>
     </SessionProvider>
   );
