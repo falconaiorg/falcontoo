798cb0ee88a90dfd1c06f1795883788c0b014e06
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-06-14 01:55:56 +0530

    refactor: Add loading page at root level.

---
+++ apps/falcontoo/app/loading.tsx
@@ -0,0 +1,9 @@
+import { LexLoading } from "@/components/loading/lex-loading";
+
+export default function Loading() {
+  return (
+    <div className="h-screen w-screen rounded-lg bg-background">
+      <LexLoading />
+    </div>
+  );
+}
