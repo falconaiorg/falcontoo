a6f03dd78b00354f658bb8b784332ab49a94b254
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-06-25 06:43:41 +0530

    feat: add unread filter

--- apps/falcontoo/app/(base)/library/article-list.tsx
+++ apps/falcontoo/app/(base)/library/article-list.tsx
@@ -1,26 +1,59 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { url } from "@/urls";
 import { ArticleCard } from "./article-card";
 import { ArticleWithContent } from "@falcon/lib/server/next/article";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { cn } from "@/lib/utils";
+import { fonts } from "@falcon/lib/fonts";
+import { Badge } from "@/components/ui/badge";
 
 export const ArticleList = ({
   articles,
 }: {
   articles: ArticleWithContent[];
 }) => {
+  const [animationParent] = useAutoAnimate();
+  const [unread, setUnread] = useState(false);
+  const filteredArticles = unread
+    ? articles.filter((a) => a.readingProgress === 0)
+    : articles;
+  const length = filteredArticles.length;
+
   return (
-    <div className="mt-8 flex flex-col gap-4 pb-96">
-      {articles.map((article) => (
-        <Link
-          href={url.reader.read({ articleId: article.id })}
-          key={article.id}
+    <div className="flex flex-col space-y-4">
+      <div className="flex flex-row items-center justify-between space-x-2">
+        <div
+          className={cn("p-2 text-xs", fonts.serif.lora, {
+            "rounded bg-secondary": unread,
+          })}
         >
-          <ArticleCard article={article} />
-        </Link>
-      ))}
-      <div className="mt-10 text-center text-xl font-bold text-slate-700">
-        {"That's all folks!"}
+          {length} articles
+        </div>
+        <Button
+          className="hover:bg-inherit hover:text-inherit"
+          variant={unread ? "secondary" : "outline"}
+          size={"sm"}
+          onClick={() => setUnread((unread) => !unread)}
+        >
+          unread
+        </Button>
+      </div>
+      <div className="mt-8 flex flex-col gap-4 pb-96" ref={animationParent}>
+        {filteredArticles.map((article) => (
+          <Link
+            href={url.reader.read({ articleId: article.id })}
+            key={article.id}
+          >
+            <ArticleCard article={article} />
+          </Link>
+        ))}
+        <div className="mt-10 text-center text-xl font-bold text-slate-700">
+          {"That's all folks!"}
+        </div>
       </div>
     </div>
   );
