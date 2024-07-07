0672e3928924804c6eb3c2d0a06c6844c1c78f58
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-07-07 06:10:47 +0530

    feat: Improve PWA detection logic and event handling in useUserAgent

--- apps/falcontoo/app/(app)/(base)/home/use-user-agent.ts
+++ apps/falcontoo/app/(app)/(base)/home/use-user-agent.ts
@@ -1,12 +1,14 @@
 "use client";
 import { useState, useEffect } from "react";
 import UAParser from "ua-parser-js";
+
 // Extend the Navigator interface
 declare global {
   interface Navigator {
     standalone?: boolean;
   }
 }
+
 export const useUserAgent = () => {
   const [userAgentInfo, setUserAgentInfo] = useState<UAParser.IResult | null>(
     null,
@@ -24,13 +26,35 @@ export const useUserAgent = () => {
       setIsChrome(true);
     }
 
-    // Determine if the app is running as a PWA
-    let displayMode = "browser";
-    const mqStandAlone = "(display-mode: standalone)";
-    if (navigator.standalone || window.matchMedia(mqStandAlone).matches) {
-      displayMode = "standalone";
-    }
-    setIsPWA(displayMode === "standalone");
+    // Function to check if the app is running as a PWA
+    const checkIsPWA = () => {
+      const isStandalone = window.matchMedia(
+        "(display-mode: standalone)",
+      ).matches;
+      const isFullscreen = window.matchMedia(
+        "(display-mode: fullscreen)",
+      ).matches;
+      const isMinimalUi = window.matchMedia(
+        "(display-mode: minimal-ui)",
+      ).matches;
+      const isIosPwa = window.navigator.standalone;
+
+      const isPWA = isStandalone || isFullscreen || isMinimalUi || isIosPwa;
+      return !!isPWA;
+    };
+
+    // Set initial PWA state
+    setIsPWA(checkIsPWA());
+
+    // Add event listener for changes in display mode
+    const mediaQuery = window.matchMedia("(display-mode: standalone)");
+    const handleChange = (e: MediaQueryListEvent) => setIsPWA(checkIsPWA());
+
+    // Use addEventListener instead of addListener
+    mediaQuery.addEventListener("change", handleChange);
+
+    // Cleanup function
+    return () => mediaQuery.removeEventListener("change", handleChange);
   }, []);
 
   return { userAgentInfo, isChrome, isPWA };
