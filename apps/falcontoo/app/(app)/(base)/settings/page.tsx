d93693418884eb3e777432c48259a075f1239dc3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-07-08 20:22:35 +0530

    feat: Add "Update Log" section to settings page

--- apps/falcontoo/app/(app)/(base)/settings/page.tsx
+++ apps/falcontoo/app/(app)/(base)/settings/page.tsx
@@ -3,9 +3,10 @@ import { fonts } from "@falcon/lib/fonts";
 import { getServerComponentSession } from "@falcon/lib/next-auth";
 import Link from "next/link";
 import { SignOut } from "@/components/auth/sign-out";
-import { roadmap } from "@/app/roadmap";
+import { roadmap, updateLog } from "@/app/roadmap";
 import { CONSTANTS } from "@falcon/lib/constants";
 import { getFirstName } from "@falcon/lib/utils";
+import { Separator } from "@/components/ui/separator";
 
 const { crimsonPro, lora } = fonts.serif;
 
@@ -20,9 +21,7 @@ export default async function SettingsPage() {
   return (
     <div className="flex h-full flex-col space-y-8 overflow-y-auto px-6 py-10 pb-32 text-slate-300 scrollbar-thin">
       <section className="flex flex-col space-y-3">
-        <h1 className={cn("text-3xl font-thin", crimsonPro)}>
-          Hello, {firstName}!
-        </h1>
+        <h1 className={cn("text-3xl", crimsonPro)}>Hello, {firstName}!</h1>
         <p className={cn(lora)}>Your wish, is our command.</p>
         <p className={cn(lora)}>
           {`If you face any issues or have any suggestions, feel free to to react
@@ -39,10 +38,11 @@ export default async function SettingsPage() {
         </p>
       </section>
       <section className="flex flex-col space-y-3">
-        <h1 className={cn("text-3xl font-thin", crimsonPro)}>Roadmap</h1>
+        <h1 className={cn("text-3xl font-thin text-green-500", crimsonPro)}>Roadmap</h1>
+        <Separator />
         <div className={cn("flex flex-col space-y-4", lora)}>
           <div className="flex flex-col space-y-1">
-            <h2 className={cn("text-xl text-green-500", crimsonPro)}>
+            <h2 className={cn("text-xl text-green-600", crimsonPro)}>
               In 1 Week
             </h2>
             <div className="flex flex-col space-y-1">
@@ -54,7 +54,7 @@ export default async function SettingsPage() {
             </div>
           </div>
           <div className="flex flex-col space-y-1">
-            <h2 className={cn("text-xl text-orange-500", crimsonPro)}>
+            <h2 className={cn("text-xl text-green-600", crimsonPro)}>
               In 1 Month
             </h2>
             <div className="flex flex-col space-y-1">
@@ -66,7 +66,7 @@ export default async function SettingsPage() {
             </div>
           </div>
           <div className="flex flex-col space-y-1">
-            <h2 className={cn("text-xl text-sky-500", crimsonPro)}>
+            <h2 className={cn("text-xl text-green-600", crimsonPro)}>
               Known Issues
             </h2>
             <div className="flex flex-col space-y-1">
@@ -74,6 +74,39 @@ export default async function SettingsPage() {
                 <p className={cn(lora)} key={item}>
                   {item}
                 </p>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col space-y-3">
+        <h1 className={cn("text-3xl font-thin text-blue-500", crimsonPro)}>Update Log</h1>
+        <Separator />
+        <div className={cn("flex flex-col space-y-4", lora)}>
+          <div className="flex flex-col space-y-1">
+            <div className="flex flex-col space-y-3">
+              {updateLog.map((item) => (
+                <div
+                  className={cn("flex flex-col space-y-1", lora)}
+                  key={item.date}
+                >
+                  <h2 className={cn("text-xl text-blue-600", crimsonPro)}>
+                    {item.date}
+                  </h2>
+                  <div className="flex flex-col space-y-1">
+                    {item.changes.map((change) => (
+                      <div
+                        key={change.title + change.description}
+                        className={lora}
+                      >
+                        <p className={cn("text-slate-300")}>{change.title}</p>
+                        <p className={cn("pl-3 text-sm text-slate-400")}>
+                          {change.description}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
               ))}
             </div>
           </div>
--- apps/falcontoo/app/roadmap.ts
+++ apps/falcontoo/app/roadmap.ts
@@ -1,5 +1,5 @@
 export const roadmap = {
-  soon: ["Overall Reader Enhancements", "Public Reading Lists"],
+  soon: ["Public Reading Profile", "Article Recommendations"],
   later: ["Auto-track reading progress", "Highlights", "Chat with the article"],
   backlog: [
     "Article to Podcast",
@@ -8,3 +8,30 @@ export const roadmap = {
   ],
   issues: ["Miscalculation of reading time", "Saving the article is slow"],
 };
+
+export const updateLog = [
+  {
+    date: "8th July, 2024",
+    changes: [
+      {
+        title: "Faster Article Loading",
+        description: "Saved articles now load 2x faster than before.",
+      },
+      {
+        title: "Fixed Logout Issues",
+        description:
+          "Users got an error when trying to logout. This has been fixed.",
+      },
+    ],
+  },
+  {
+    date: "6th July, 2024",
+    changes: [
+      {
+        title: "Custom Reader Fonts & Themes",
+        description:
+          "You can now choose from a variety of fonts, themes and typography options by clicking the settings icon on the top right of the reader.",
+      },
+    ],
+  },
+] as const;
